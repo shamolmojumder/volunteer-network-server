56afc4457826f3f9123cf3d06f5ad16385fcbe76
shamolmojumder <shamolmojumder14@gmail.com>
2021-10-09 13:08:11 +0600

    48_5-5 Connect database server and implement react hook form 3:00

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5055
+const express = require('express');
+const app = express();;
+const port = process.env.PORT || 5055;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
