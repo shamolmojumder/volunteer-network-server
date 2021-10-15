0eb072fe1af878c0b72f6477e084c1ed31164786
shamolmojumder <shamolmojumder14@gmail.com>
2021-10-16 00:26:46 +0600

    48_5-7 Upload events with image link to database

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5055;
 
-
 app.use(cors());
 app.use(bodyParser.json())
 
@@ -15,17 +14,26 @@ app.get('/', (req, res) => {
 })
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4xoys.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   console.log("err",err);
   const eventCollection = client.db("volunteer").collection("events");
-  // perform actions on the collection object
-  console.log("database connection successfully");
+
+  app.post('/addEvent',(req,res)=>{
+    const newEvent=req.body;
+    console.log('adding new event: ',newEvent);
+    eventCollection.insertOne(newEvent)
+    .then(result=>{
+      console.log("inserted count",result.insertedCount);
+      res.send(result.insertedCount >0)
+    })
+  })
+
+
   // client.close();
+  
 });
 
 
